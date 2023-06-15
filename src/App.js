62568cdf2f683b9779f8f514286af36c9ececce4
Sakshi <sakshimahajan174@gmail.com>
2023-06-15 09:52:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div  className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +20,6 @@ function App() {
       </header>
     </div>
   );
-}
+ }
 
 export default App;
